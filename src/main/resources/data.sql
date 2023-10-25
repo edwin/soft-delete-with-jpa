533098c07eae0552e79545d2f5a1d75dffc1d1bb
edwin <edwinkun@gmail.com>
2023-10-25 17:29:46 +0700

    adding h2 db :snail:

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO t_user (user_id, user_name, status) VALUES (100, 'edwin', 1);
+INSERT INTO t_user (user_id, user_name, status) VALUES (101, 'username02', 1);
+INSERT INTO t_user (user_id, user_name, status) VALUES (102, 'something', 1);
